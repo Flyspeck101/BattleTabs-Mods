c61deba0e973b514313df08d4f3ab3eab5bf04dc
Flyspeck101 <yeetersdeleters12345@gmail.com>
2021-11-17 17:44:30 +1100

    Update url.js

--- url.js
+++ url.js
@@ -1,9 +1,11 @@
-let documentURL = window.location.href;
-let replayLinkFound = false;
-let battleURL = "";
-let params = documentURL.search;
-if (params != "") {
-  replayLinkFound = true;
-  battleURL = params.slice(8);
-}
-if (replayLinkFound) document.getElementById("bt").src = battleURL;
+setTimeout(() => {
+  let documentURL = window.location.href;
+  let replayLinkFound = false;
+  let battleURL = "";
+  let params = documentURL.search;
+  if (params != "") {
+    replayLinkFound = true;
+    battleURL = params.slice(8);
+  }
+  if (replayLinkFound) document.getElementById("bt").src = battleURL;
+}, 500);
