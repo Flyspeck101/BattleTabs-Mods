b1de863352599a000365bcc133f6405f2a496c95
Flyspeck101 <yeetersdeleters12345@gmail.com>
2021-11-17 17:35:45 +1100

    Update url.js

--- url.js
+++ url.js
@@ -1,10 +1,9 @@
 let documentURL = window.location.href;
 let replayLinkFound = false;
-let battleURL = (() => {
-  if (new URLSearchParams(documentURL.searchParams).get("replay")) {
-    replayLinkFound = true;
-    return new URLSearchParams(documentURL.searchParams).get("replay");
-  }
-  return "";
-})();
+let battleURL = "";
+let params = new URLSearchParams(documentURL.searchParams);
+if (params.get("replay")) {
+  replayLinkFound = true;
+  battleURL = params.get("replay")
+}
 if (replayLinkFound) document.getElementById("bt").src = battleURL;
